e07807ce207c2022515ced8f1dc62f4695a48cd6
Mani Ka <mani@talview.com>
2024-09-20 12:51:27 -0700

    Fail on err

--- src/env.ts
+++ src/env.ts
@@ -6,9 +6,13 @@ import Kv from './kv'
 export async function setup(prefix: string): Promise<void> {
   const items = filter(Object.keys(process.env), (i: string) => startsWith(i, `${prefix}_`) || startsWith(i, `KV_`))
   await PromiseExtended.map(items, async (k: string): Promise<void> => {
-    const key = get(k.split(`${prefix}_`), '1') || get(k.split(`KV_`), '1')
-    const value: string = await Kv.getSecret(prefix, key)
-    core.exportVariable(key, value)
-    core.setSecret(`${value}`)
+    try {
+      const key = get(k.split(`${prefix}_`), '1') || get(k.split(`KV_`), '1')
+      const value: string = await Kv.getSecret(prefix, key)
+      core.exportVariable(key, value)
+      core.setSecret(`${value}`)
+    } catch (err) {
+      core.setFailed(`err: ${err}`)
+    }
   })
 }
