92cc64ecc5fe576b89dbc50bda2c1de6f632e8e6
Mani Ka <mani@talview.com>
2024-09-23 19:25:13 -0700

    Mapping to target env

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,8 @@ export async function setup(prefix: string): Promise<void> {
     try {
       const key = get(k.split(`${prefix}_`), '1') || get(k.split(`KV_`), '1')
       const value: string = await Kv.getSecret(prefix, key)
-      core.exportVariable(process.env[key]!, value)
+      const target = process.env[k]!
+      core.exportVariable(target, value)
       core.setSecret(`${value}`)
     } catch (err) {
       core.setFailed(`err: ${err}`)
